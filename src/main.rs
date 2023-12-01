dbea9a84d8e9361d9ffb119942efcf6587501585
timojokinen <timojannejokinen@gmail.com>
2023-12-01 23:20:34 +0100

    cleanup main

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 mod attacks;
 mod castling_rights;
+mod cmove;
 mod fen;
+mod piece;
 mod position;
 mod util;
 
@@ -12,9 +14,5 @@ fn main() {
     init_tables();
 
     let position = Position::from("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
-    let pieces = position.pieces[1];
-    print_bitboard(pieces);
-    iterate_bits(pieces, |idx| {
-        println!("{}", idx);
-    })
+    position.generate_moves();
 }
